894627aafd7f41f229f5029ce59bf3cfa8b5a5e8
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2025-01-09 16:42:57 +0530

    feat: events, footer sections

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,6 +13,11 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        roboto: ["Roboto", 'serif'],
+        space_mono: ["Space Mono", 'serif'],
+        roboto_mono: [ "Roboto Mono", 'serif']
+      }
     },
   },
   plugins: [],
