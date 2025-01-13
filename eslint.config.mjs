7aefcd8695bedefa2c940ccfaaa16c9755d43408
Keshav-Aneja <keshav.aneja09@gmail.com>
2025-01-13 19:52:21 +0530

    chore: eslint config change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,13 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "no-unused-vars": "off",
-    },
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+export default tseslint.config({
+  rules: {
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
   },
-];
-
-export default eslintConfig;
+});
+// export default eslintConfig;
