1ecaee7ff0900febe133568525d16d96ec69c5c7
userrounakk <contact@userrounakk.com>
2025-01-14 21:28:58 +0530

    fix: chatbot added to layout and about section content changed

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -15,25 +15,12 @@ export default function AboutSection() {
       <div className="border-r border-b border-border"></div>
       <div className="border-r border-b border-border p-8">
         <p className="font-mono text-sm leading-relaxed opacity-80">
-          Vellore Institute of Technology (VIT) stands as one of India's premier
-          institutions, recognized as an "Institution of Eminence" by the
-          Government of India. Founded with the vision of providing an "Abode
-          for Learning and Growth," VIT continues to uphold its principles of
-          excellence in education and holistic development.
+          Vellore Institute of Technology (VIT) stands as one of India's premier institutions, recognized as an "Institution of Eminence" by the Government of India. Founded with the vision of providing an "Abode for Learning and Growth", VIT continues to uphold its principles of excellence in education and holistic development.
           <br />
           <br />
-          The university is renowned not only for its academic rigor but also
-          for fostering an environment conducive to personal and professional
-          growth. With industrial visits, expert guest lectures, and numerous
-          hackathons, VIT equips students to tackle real-world challenges with
-          innovative solutions. <br />
+          The university is renowned not only for its academic rigor but also for fostering an environment conducive to personal and professional growth. With industrial visits, expert guest lectures, and numerous hackathons, VIT equips students to tackle real-world challenges with innovative solutions. <br />
           <br />
-          VIT’s vibrant ecosystem includes several prominent clubs and chapters,
-          offering students a platform to showcase their creativity and
-          technical acumen. Ranked the No. 1 private institution in India for
-          Innovation in 2019 by the Government of India, VIT strives to provide
-          quality education on par with international standards, with the
-          ultimate goal of contributing to societal welfare and global progress.
+          VIT's vibrant ecosystem includes several prominent clubs and chapters, offering students a platform to showcase their creativity and technical acumen. Ranked the No. 1 private institution in India for Innovation in 2019 by the Government of India, VIT strives to provide quality education on par with international standards, with the ultimate goal of contributing to social welfare and global progress. 
         </p>
       </div>
       <div className="border-b border-border flex flex-col justify-between">
@@ -74,29 +61,13 @@ export default function AboutSection() {
       </div>
       <div className="border-b border-l border-border p-8">
         <p className="font-mono text-sm leading-relaxed opacity-80">
-          Home is where the heart is, but the college journey can often feel
-          unmoored. This is where Student Welfare takes center stage—a dedicated
-          team comprising the Director of Students' Welfare, Assistant
-          Directors, Student Council Members, and Programme Representatives, all
-          steadfastly committed to the holistic well-being of every VIT-ian.
+          Home is where the heart is, but the college journey can often feel unmoored. This is where Student Welfare takes center stage—a dedicated team comprising the Director of Students' Welfare, Assistant Directors, Student Council Members, and Programme Representatives, all steadfastly committed to the holistic well-being of every VIT-ian.
           <br />
           <br />
-          From the anticipation of induction to the heartfelt goodbyes of
-          graduation, Student Welfare accompanies you through every milestone of
-          your journey. We are the quiet counsel during midnight study sessions,
-          the reassuring presence after rigorous exams, and the shared elation
-          of cultural festivities. Beyond fostering academic success, we
-          endeavor to enrich your experience by nurturing personal growth,
-          social engagement, and cherished memories.
+          From the anticipation of induction to the heartfelt goodbyes of graduation, Student Welfare accompanies you through every milestone of your journey. We are the quiet counsel during midnight study sessions, the reassuring presence after rigorous exams, and the shared elation of cultural festivities. Beyond fostering academic success, we endeavor to enrich your experience by nurturing personal growth, social engagements, and cherished memories.
           <br />
           <br />
-          Through vibrant clubs, chapters, OD’s, facilitation of external
-          participation, and budgetary provisions, we empower you to excel and
-          explore boundless opportunities. As the voice of the student body, the
-          Student Council ensures your aspirations are championed. At Student
-          Welfare, we are your unwavering pillar of support, dedicated to making
-          your VIT journey an extraordinary and transformative chapter of your
-          life.
+          Through vibrant clubs, chapters, OD’s, facilitation of external participation, and budgetary provisions, we empower you to excel and explore boundless opportunities. As the voice of the student body, the Student Council ensures your aspirations are championed. At Student Welfare, we are your unwavering pillar of support, dedicated to making your VIT journey an extraordinary and transformative chapter of your life.
         </p>
       </div>
       <div className="border-r border-b border-border flex flex-col p-4 justify-end items-srart h-[10rem] min-h-fit">
@@ -108,26 +79,13 @@ export default function AboutSection() {
       <div className="border-r border-b border-border"></div>
       <div className="border-r border-b border-border p-8">
         <p className="font-mono text-sm leading-relaxed opacity-80">
-          Yantra, the annual flagship techno-management fest of VIT, is a
-          distinguished celebration of innovation and large-scale collaboration.
-          Spanning an entire week, the fest garners participation from over
-          35,000 students, establishing itself as a dynamic platform for
-          experiential learning and creative exploration. Yantra showcases a
-          meticulously curated lineup of workshops, technical talks, hackathons,
-          and competitions, all organized by the diverse clubs and chapters of
-          VIT.
+          Yantra, the annual flagship techno-management fest of VIT, is a distinguished celebration of innovation and large-scale collaboration. Spanning an entire week, Yantra’25 garners participation from over 35,000 students. Now in its 5th edition, Yantra showcases a meticulously curated lineup of workshops, technical talks, hackathons, and competitions, all organized by the diverse clubs and chapters of VIT.
           <br />
           <br />
-          At the core of this landmark event lies Central Hack—VIT's biggest and
-          most prestigious hackathon. This transformative platform unites
-          individuals from diverse domains with a shared vision to address
-          real-world challenges through ingenious and interdisciplinary
-          solutions. It fosters critical problem-solving, enhances technical and
-          managerial competencies, and facilitates meaningful networking
-          opportunities. Yantra stands as a testament to VIT's unwavering
-          commitment to technological innovation and managerial excellence,
-          offering participants a profoundly enriching and transformative
-          experience—all at no cost.
+          At the core of this landmark event lies Central Hack—VIT's biggest and most prestigious hackathon. This transformative platform unites individuals from diverse domains with a shared vision to address real-world challenges through ingenious and interdisciplinary solutions. It fosters critical problem-solving, enhances technical and managerial competencies, and facilitates meaningful networking opportunities. Yantra stands as a testament to VIT's unwavering commitment to technological innovation and managerial excellence, offering participants a profoundly enriching and transformative experience—all at no cost.
+          <br />
+          <br />
+          The 5th edition of Yantra, revolves around Artificial Intelligence (AI) with the objective of advancing the Sustainable Development Goals. This event serves as an incubator for young visionaries to engage with avant-garde ideas, hone technical expertise, and craft solutions that address global challenges across economic, social, and environmental spheres. 
         </p>
       </div>
       <div className="border-b border-border flex flex-col justify-between">
