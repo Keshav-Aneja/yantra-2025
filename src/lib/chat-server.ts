211ced6b31ea1c2c1894359d0284fadb9b805dac
Aryan Bharti <87516386+woaitsAryan@users.noreply.github.com>
2025-01-30 18:00:20 +0530

    debug: chatbot error

--- src/lib/chat-server.ts
+++ src/lib/chat-server.ts
@@ -14,11 +14,13 @@ export const askBot = async (question: string) => {
     })
 
     if (res.data.error) {
+      console.error(res.data.error)
       return { status: "error", message: res.data.error }
     }
 
     return { status: "success", message: res.data.result }
   } catch (error) {
+    console.error(error);
     return { status: "error", message: "Internal Server Error" }
   }
 }
