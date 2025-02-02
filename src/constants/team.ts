c07305b0403bb9777c2bb2f53ee0e31381fd471a
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2025-02-02 21:36:35 +0530

    chore: teams page

--- src/constants/team.ts
+++ src/constants/team.ts
@@ -135,6 +135,21 @@ export const teamPageData = [
     ],
   },
   {
+    title: "Faculty Advisors",
+    people: [
+      {
+        name: "Sharmila N",
+        img: "/images/team/sharmila.jpg",
+        title: "Faculty Advisor"
+      },
+      {
+        name: "Aarthi S.L",
+        img: "/images/team/aarthy.jpg",
+        title: "Faculty Advisor"
+      }
+    ]
+  },
+  {
     title: "Student Organizers",
     people: [
       {
@@ -194,4 +209,273 @@ export const teamPageData = [
       },
     ],
   },
+  {
+    title: "Student Advisors",
+    people: [
+      {
+        name: "Devabathini Chinmay",
+        img: "/images/team/chinmay.png",
+        title: "Student Advisor"
+      },
+      {
+        name: "Sanjeeva Reddy S",
+        img: "/images/team/sanjeeva.png",
+        title: "Student Advisor"
+      }
+    ]
+  }
 ];
+
+export const studentCouncil = [
+  { domain: "Design", name: "SMRITI KUMARI" },
+  { domain: "Design", name: "NAVANEEKRISHNAN" },
+  { domain: "Design", name: "JAGAN M" },
+  { domain: "Design", name: "POTNURU SAI VIGNESH" },
+  { domain: "Design", name: "NAMAN JAIN M" },
+  { domain: "Design", name: "V SUNDARA MURTHY" },
+  { domain: "Design", name: "A SRUTHI" },
+  { domain: "Design", name: "ASHMITA RAVI" },
+  { domain: "Documentation", name: "ARPIT TIWARI" },
+  { domain: "Documentation", name: "AADITYA RAJ" },
+  { domain: "Events and Documentation", name: "ISHAN JAIN" },
+  { domain: "Events and Documentation", name: "ROHAN AGRAWAL" },
+  { domain: "Events and Documentation", name: "KARTHIK POOVENTHAN R" },
+  { domain: "Events and Documentation", name: "ABIR PATHANIA" },
+  { domain: "Events and Documentation", name: "SAKTHI PRIYA S" },
+  { domain: "Events and Documentation", name: "S HARRIPRIYA" },
+  { domain: "Events and Documentation", name: "BRYAN ANTHONY GONSALVES" },
+  { domain: "Events and Documentation", name: "SARAVANAN D" },
+  { domain: "Events and Documentation", name: "PARTH SHARMA" },
+  { domain: "Events and Documentation", name: "ADITYA PRAMODU PILLAI" },
+  { domain: "Events and Documentation", name: "A S PRADNYA" },
+  { domain: "Events and Documentation", name: "A DEEPTHI" },
+  { domain: "Events and Documentation", name: "AGRAWAL DHRUV MANOJ" },
+  { domain: "Events and Documentation", name: "SHREYAS SARNATH" },
+  { domain: "Events and Documentation", name: "PANNAGA RAVINDRA HEGDE" },
+  { domain: "Events and Documentation", name: "SHREYA CHAKRABORTY" },
+  { domain: "Events and Documentation", name: "ADITI SHARMA" },
+  { domain: "Events and Documentation", name: "SHUBHAM GANGULY" },
+  { domain: "Events and Documentation", name: "HARI VARDARAJ S" },
+  { domain: "Events and Documentation", name: "JOASH THOMAS ABRAHAM" },
+  { domain: "Events and Documentation", name: "HEMA G" },
+  { domain: "Events and Documentation", name: "SHREYA SURESH T P" },
+  { domain: "Events and Documentation", name: "AMAR HAMDAN" },
+  { domain: "Events and Documentation", name: "KEERTHANA T" },
+  { domain: "Events and Documentation", name: "SANJAY A" },
+  { domain: "Events and Documentation", name: "KARKI SOHAN" },
+  { domain: "Guest Care", name: "MOHIT PANKAJ MUNDRA" },
+  { domain: "Guest Care", name: "JEFIN MATHEW KURIAN" },
+  { domain: "Guest Care", name: "HARSHIT GAUR" },
+  { domain: "Guest Care", name: "KALKI ESHWAR D" },
+  { domain: "Guest Care", name: "KESHAV S KAUSHISH" },
+  { domain: "Guest Care", name: "AAYUSH NEGI" },
+  { domain: "Guest Care", name: "PRITHA SHARMA" },
+  { domain: "Guest Care", name: "SIDDHARTH MAHINDRAKAR" },
+  { domain: "Halls and Refereshments", name: "BOGOLU MANI PREETHAM" },
+  { domain: "Halls and Refereshments", name: "G VASUDHA" },
+  { domain: "Halls and Refereshments", name: "ALAPATI PRAJITH" },
+  { domain: "Halls and Refereshments", name: "ABISHEK R" },
+  { domain: "Halls and Refereshments", name: "ADITYA SHARDA" },
+  { domain: "Halls and Refereshments", name: "PITOLWALA UMMUL KIRAAM" },
+  { domain: "Halls and Refereshments", name: "AMAL SUMESH" },
+  { domain: "Halls and Refereshments", name: "RANJITHA" },
+  { domain: "Halls and Refereshments", name: "NAVYA NEHAL" },
+  { domain: "Halls and Refereshments", name: "KAAVYA N DEKHANE" },
+  { domain: "Halls and Refereshments", name: "BHARGAV MAHANTA" },
+  { domain: "Halls and Refereshments", name: "DAKSH GAMBHIR" },
+  { domain: "Halls and Refereshments", name: "VANSH ARYA" },
+  { domain: "Halls and Refereshments", name: "G YASWANTHKUMAR" },
+  { domain: "Halls and Refereshments", name: "DHRUVIE KHANNA" },
+  { domain: "Halls and Refereshments", name: "AKSHAYA CHIDAMBARA RAJAN" },
+  { domain: "Purchase", name: "CHAMAN TAHSEEN K N" },
+  { domain: "Purchase", name: "SREEMATHI M" },
+  { domain: "Purchase", name: "SIDDHARTH DAS" },
+  { domain: "Purchase", name: "ADITYA PRABHU" },
+  { domain: "Purchase", name: "MOHAMMED AFAAN SHERIEF" },
+  { domain: "Purchase", name: "SWATHI A" },
+  { domain: "Purchase", name: "THEJAL T JOHN" },
+  { domain: "Registeration", name: "SNEHA SHREE R" },
+  { domain: "Registeration", name: "RAJPUROHIT NIKITA" },
+  { domain: "Registeration", name: "SHARANYA PAL" },
+  { domain: "Registeration", name: "PISE GAURAV TIKARAM" },
+  { domain: "Registeration", name: "SHEETAL REDDY" },
+  { domain: "Registeration", name: "AFRIN KHANAM" },
+  { domain: "Registeration", name: "ANMOL CHAUDHARY" },
+  { domain: "Registeration", name: "EYAZHINI K S" },
+  { domain: "Registeration", name: "AYUSHMITA GUPTA" },
+  { domain: "Registeration", name: "ASWINKARTHICK K T" },
+  { domain: "Registeration", name: "KAMARAJU VYSHNAVI DEVI" },
+  { domain: "Registeration", name: "ANKIT SUBEDI" },
+  { domain: "Registeration", name: "DEVANANDA P A" },
+  { domain: "Sponsorship and Finance", name: "KOUSHIK R" },
+  { domain: "Sponsorship and Finance", name: "CHAKRAVARTHY S" },
+  { domain: "Sponsorship and Finance", name: "HARSHVARDHAN K" },
+  { domain: "Sponsorship and Finance", name: "RAJAT AGRAWAL" },
+  { domain: "Sponsorship and Finance", name: "SRUTHI SURESH" },
+  { domain: "Sponsorship and Finance", name: "SRUTHI K" },
+  { domain: "Sponsorship and Finance", name: "E PANIMALAR" },
+  { domain: "Sponsorship and Finance", name: "ATMAJEET SAMAL" },
+  { domain: "Sponsorship and Finance", name: "ARTHUR LESLIE" },
+  { domain: "Sponsorship and Finance", name: "KAJAL R" },
+  { domain: "Tech", name: "PATIL VEDANT RAJENDRA" },
+  { domain: "Tech", name: "ROUNAK AGRAWAL" },
+  { domain: "Tech", name: "KESHAV ANEJA" },
+  { domain: "Tech", name: "ARYAN BHARTI" },
+  { domain: "Tech", name: "SANJAI SENTHIL S" }
+];
+
+export const studentCouncilWithDomains = [
+  {
+    "title": "Design",
+    "people": [
+      "SMRITI KUMARI",
+      "NAVANEEKRISHNAN",
+      "JAGAN M",
+      "POTNURU SAI VIGNESH",
+      "NAMAN JAIN M",
+      "V SUNDARA MURTHY",
+      "A SRUTHI",
+      "ASHMITA RAVI"
+    ]
+  },
+  {
+    "title": "Documentation",
+    "people": [
+      "ARPIT TIWARI",
+      "AADITYA RAJ"
+    ]
+  },
+  {
+    "title": "Events and Documentation",
+    "people": [
+      "ISHAN JAIN",
+      "ROHAN AGRAWAL",
+      "KARTHIK POOVENTHAN R",
+      "ABIR PATHANIA",
+      "SAKTHI PRIYA S",
+      "S HARRIPRIYA",
+      "BRYAN ANTHONY GONSALVES",
+      "SARAVANAN D",
+      "PARTH SHARMA",
+      "ADITYA PRAMODU PILLAI",
+      "A S PRADNYA",
+      "A DEEPTHI",
+      "AGRAWAL DHRUV MANOJ",
+      "SHREYAS SARNATH",
+      "PANNAGA RAVINDRA HEGDE",
+      "SHREYA CHAKRABORTY",
+      "ADITI SHARMA",
+      "SHUBHAM GANGULY",
+      "HARI VARDARAJ S",
+      "JOASH THOMAS ABRAHAM",
+      "HEMA G",
+      "SHREYA SURESH T P",
+      "AMAR HAMDAN",
+      "KEERTHANA T",
+      "SANJAY A",
+      "KARKI SOHAN"
+    ]
+  },
+  {
+    "title": "Guest Care",
+    "people": [
+      "MOHIT PANKAJ MUNDRA",
+      "JEFIN MATHEW KURIAN",
+      "HARSHIT GAUR",
+      "KALKI ESHWAR D",
+      "KESHAV S KAUSHISH",
+      "AAYUSH NEGI",
+      "PRITHA SHARMA",
+      "SIDDHARTH MAHINDRAKAR"
+    ]
+  },
+  {
+    "title": "Halls and Refereshments",
+    "people": [
+      "BOGOLU MANI PREETHAM",
+      "G VASUDHA",
+      "ALAPATI PRAJITH",
+      "ABISHEK R",
+      "ADITYA SHARDA",
+      "PITOLWALA UMMUL KIRAAM",
+      "AMAL SUMESH",
+      "RANJITHA",
+      "NAVYA NEHAL",
+      "KAAVYA N DEKHANE",
+      "BHARGAV MAHANTA",
+      "DAKSH GAMBHIR",
+      "VANSH ARYA",
+      "G YASWANTHKUMAR",
+      "DHRUVIE KHANNA",
+      "AKSHAYA CHIDAMBARA RAJAN"
+    ]
+  },
+  {
+    "title": "Purchase",
+    "people": [
+      "CHAMAN TAHSEEN K N",
+      "SREEMATHI M",
+      "SIDDHARTH DAS",
+      "ADITYA PRABHU",
+      "MOHAMMED AFAAN SHERIEF",
+      "SWATHI A",
+      "THEJAL T JOHN"
+    ]
+  },
+  {
+    "title": "Registeration",
+    "people": [
+      "SNEHA SHREE R",
+      "RAJPUROHIT NIKITA",
+      "SHARANYA PAL",
+      "PISE GAURAV TIKARAM",
+      "SHEETAL REDDY",
+      "AFRIN KHANAM",
+      "ANMOL CHAUDHARY",
+      "EYAZHINI K S",
+      "AYUSHMITA GUPTA",
+      "ASWINKARTHICK K T",
+      "KAMARAJU VYSHNAVI DEVI",
+      "ANKIT SUBEDI",
+      "DEVANANDA P A"
+    ]
+  },
+  {
+    "title": "Sponsorship and Finance",
+    "people": [
+      "KOUSHIK R",
+      "CHAKRAVARTHY S",
+      "HARSHVARDHAN K",
+      "RAJAT AGRAWAL",
+      "SRUTHI SURESH",
+      "SRUTHI K",
+      "E PANIMALAR",
+      "ATMAJEET SAMAL",
+      "ARTHUR LESLIE",
+      "KAJAL R"
+    ]
+  },
+  {
+    "title": "Tech",
+    "people": [
+      "PATIL VEDANT RAJENDRA",
+      "ROUNAK AGRAWAL",
+      "KESHAV ANEJA",
+      "ARYAN BHARTI",
+      "SANJAI SENTHIL S"
+    ]
+  }
+]
+
+/*
+<div className={"w-full flex flex-col items-center gap-2"}>
+  <div className={`text-sm text-white bg-white/30 w-fit p-1 px-2 text-center rounded-xl font-armstrong font-light `}>{section.title}</div>
+  <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-[2px]" key={index}>
+      {section.people.map((person, _i) => (
+          <div key={`${index}-${_i}`} className={"bg-white font-medium text-black flex flex-col justify-between py-5 px-3"}>
+              <div className={"text-center"}>{person}</div>
+          </div>
+      ))}
+  </div>
+</div>
+* */
